e33ab412bcca216b22b649b44c8b06ade4f00da0
isaac <isaac.j.mayo@gmail.com>
2025-04-29 14:54:07 -0700

    Adding test autonomous mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Paths/operationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Paths/operationTest.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.Paths;
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import org.firstinspires.ftc.teamcode.Assemblies.ascentMechanism;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@TeleOp(name = "Test", group = "Concept")
+public class operationTest extends LinearOpMode {
+ascentMechanism arm = new ascentMechanism(hardwareMap, telemetry);
+    @Override
+    public void runOpMode() {
+arm.armMove((int) gamepad1.left_stick_x);
+    }
+}
